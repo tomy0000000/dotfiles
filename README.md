13654b484e77894d8d5cadfbcd357ea2be15effa
Tomy Hsieh <tomy0000000@gmail.com>
2021-03-08 10:55:45 +0800

    Update alias

--- README.md
+++ README.md
@@ -5,19 +5,8 @@ Most of the function is still under construction, use at your own risk.
 
 ## Install
 
-### Headless (draft)
-
-```shell
-# curl
-curl -fsSL https://install.tomy.tech | bash
-# wget
-wget -O - https://install.tomy.tech | bash
-```
-
-### Manual
-
 ```sh
 git clone https://github.com/tomy0000000/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
-make all # draft
+make [feature]
 ```
--- scripts/alias.sh
+++ scripts/alias.sh
@@ -3,23 +3,27 @@ echo "⚡️ Installing alias"
 
 # Check existed alias
 if [ -d "${HOME}/.alias" ]; then
-    for alias in alias/*; do
-        if [ -f "${HOME}/.alias/${alias}" ]; then
-            echo "${alias} already exists, backed up."
-            mv "${HOME}/${alias}" "${HOME}/${alias}.backup"
-        fi
-    done
+    # for alias in alias/*; do
+    #     if [ -f "${HOME}/.alias/${alias}" ]; then
+    #         echo "${alias} already exists, backed up."
+    #         mv "${HOME}/${alias}" "${HOME}/${alias}.backup"
+    #     fi
+    # done
+    echo ".alias already exists, backed up."
+    mv "${HOME}/.alias" "${HOME}/.alias.backup"
 else
-    mkdir -p "${HOME}/.alias"
+    # mkdir -p "${HOME}/.alias"
+    true
 fi
 
 # Stow the alias
-stow --target "${HOME}/.alias" "alias"
+# stow --target "${HOME}/.alias" "alias"
+ln -s "$(realpath alias)" "${HOME}/.alias"
 
-# Add source file
+# Add source file line in rc file
 shellrc="${HOME}/.$(echo "${SHELL}" | cut -d'/' -f'3')rc"
 cat >> "${shellrc}" << "EOF"
-for alias in .alias/*; do
+for alias in ${HOME}/.alias/*; do
   source "${alias}"
 done
 EOF
