192226e2870b97e64dbd9adbda1c595990b2abb8
Tomy Hsieh <tomy0000000@gmail.com>
2021-05-14 00:06:38 +0800

    💅🏻 style: styling & cleanup

--- scripts/alias.sh
+++ scripts/alias.sh
@@ -4,21 +4,11 @@ echo "⚡️ Installing alias"
 
 # Check existed alias
 if [ -d "${HOME}/.alias" ]; then
-    # for alias in alias/*; do
-    #     if [ -f "${HOME}/.alias/${alias}" ]; then
-    #         echo "${alias} already exists, backed up."
-    #         mv "${HOME}/${alias}" "${HOME}/${alias}.backup"
-    #     fi
-    # done
     echo ".alias already exists, backed up."
     mv "${HOME}/.alias" "${HOME}/.alias.backup"
 else
-    # mkdir -p "${HOME}/.alias"
-    true
-fi
 
 # Stow the alias
-# stow --target "${HOME}/.alias" "alias"
 ln -s "$(realpath alias)" "${HOME}/.alias"
 
 # Add source file line in rc file
