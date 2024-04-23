84cfa7b6ba81c3146fefe851a9463cfd098b7fba
Tomy Hsieh <git@tomy.me>
2024-04-23 15:08:04 -0400

    ✨ feat: `kubectl` aliases

---
+++ unix/alias/kubectl.sh
@@ -0,0 +1,29 @@
+alias k='kubectl'
+
+# Apply
+alias kaf='kubectl apply -f .'
+
+# Config
+alias kc='kubectl config'
+alias kcctg='kubectl config get-contexts'
+alias kcctu='kubectl config use-context'
+alias kcctc='kubectl config current-context'
+alias kcctd='kubectl config delete-context'
+alias kcclg='kubectl config get-clusters'
+alias kccld='kubectl config delete-cluster'
+alias kcug='kubectl config get-users'
+alias kcud='kubectl config delete-user'
+
+alias kdf='kubectl delete -f .'
+alias kdp='kubectl describe pod'
+
+# Get
+alias kgd='kubectl get deployments'
+alias kge='kubectl get events'
+alias kgn='kubectl get nodes'
+alias kgp='kubectl get pods'
+alias kgs='kubectl get services'
+
+# Log
+alias kl='kubectl logs'
+alias klf='kubectl logs -f '
