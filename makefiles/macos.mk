cabf28b26f3d1f5e9ec1adde912fee33a5a1d4a3
Tomy Hsieh <git@tomy.me>
2024-06-03 01:33:22 -0700

    🛠 fix: Cleanup macos makefile

--- makefiles/macos.mk
+++ makefiles/macos.mk
@@ -7,18 +7,20 @@ macos-mas: macos-brew
 macos-core: macos-brew macos-mas
 	echo "🖥 Operating System: macOS"
 	exists realpath || brew install coreutils
-	
+
 macos-handful:
-	brew bundle --no-lock --file "brewfiles/handful.Brewfile"
+	brew bundle --no-lock --file 'brewfiles/handful.Brewfile'
 
 macos-nano: ;
 
 macos-stow: macos-brew
 	exists stow || brew install stow
 
+# This is a full suite of git plugins and configurations
+# Most other target that requires git don't need this
 macos-git: macos-brew
 	exists git || brew install git git-extras
-	
+
 macos-touch-id-sudo:
 	xscript "scripts/macos-touch-id-sudo.sh"
 
@@ -26,7 +28,7 @@ macos-service-workflow: macos-brew
 	exists stow || brew install stow
 	stow --target "${HOME}/Library/Services" 'macos-services'
 
-macos-chromium-app-icon:
+macos-chromium-app-icon: macos-brew
 	exists fileicon || brew install fileicon
 	xscript "scripts/macos-chromium-app-icon.sh"
 
