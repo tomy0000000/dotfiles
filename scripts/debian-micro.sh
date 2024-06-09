1c67ccad9e2d4db8ec65d7f72688d04d0413242f
Tomy Hsieh <git@tomy.me>
2024-06-09 02:43:43 -0700

    🛠 fix: `micro` module (#4)

--- scripts/debian-micro.sh
+++ scripts/debian-micro.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -euo pipefail
 
+SCRIPT_DIR=$(dirname "$0")
+ABSOLUTE_SCRIPT_DIR=$(realpath "${SCRIPT_DIR}")
+
 # Install micro to current directory
+cd "${ABSOLUTE_SCRIPT_DIR}"
 bash -c "$(curl -fsSL https://getmic.ro)"
 
 # Move micro to /usr/bin
-if [ -f micro ]; then
+if [ -f "${ABSOLUTE_SCRIPT_DIR}/micro" ]; then
     sudo mv micro /usr/bin/micro
 else
     echo "micro installation failed"
