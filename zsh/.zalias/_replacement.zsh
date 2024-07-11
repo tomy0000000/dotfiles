5229ed70a998f83655a74de746e86562698ec8d9
Tomy Hsieh <git@tomy.me>
2024-07-11 01:16:24 -0700

    🗄 chore: Add alias to original bin

--- zsh/.zalias/_replacement.zsh
+++ zsh/.zalias/_replacement.zsh
@@ -5,6 +5,7 @@ alias grm="$(which rm)" # original rm
 alias rm="trash"
 
 # Use colorized cat and less
-alias gcat="$(which cat)" # original cat
+alias gcat="$(which cat)"   # original cat
+alias gless="$(which less)" # original less
 alias cat="ccat"
 alias less="cless"
