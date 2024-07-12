c8f483ac599edc8b2c765bd04a671b5099e632d8
Tomy Hsieh <git@tomy.me>
2024-07-11 23:15:44 -0700

    ✨ feat: Replace only if bin exists

--- zsh/.zalias/_replacement.zsh
+++ zsh/.zalias/_replacement.zsh
@@ -1,11 +1,19 @@
 #!/bin/zsh
 
 # Use trash instead of rm on local
-alias grm="$(which rm)" # original rm
-alias rm="trash"
+if command -v "trash" &>/dev/null; then
+    alias grm="$(which rm)" # original rm
+    alias rm="trash"
+fi
 
-# Use colorized cat and less
-alias gcat="$(which cat)"   # original cat
-alias gless="$(which less)" # original less
-alias cat="ccat"
-alias less="cless"
+# Use colorized cat
+if command -v "ccat" &>/dev/null; then
+    alias gcat="$(which cat)" # original cat
+    alias cat="ccat"
+fi
+
+# Use colorized less
+if command -v "cless" &>/dev/null; then
+    alias gless="$(which less)" # original less
+    alias less="cless"
+fi
