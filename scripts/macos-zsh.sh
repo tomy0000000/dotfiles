71007ff5f14640b875473f28c68c46e8fe19d628
Tomy Hsieh <git@tomy.me>
2024-07-11 23:10:20 -0700

    🛠 fix: prevent stowing zsh misc

--- scripts/macos-zsh.sh
+++ scripts/macos-zsh.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 # Change default shell to zsh
-echo "📟 Changing default shell to zsh"
-chsh -s "/bin/zsh"
+
+# Test if zsh is already default shell
+if [ "$SHELL" = "/bin/zsh" ]; then
+    echo "📟 Zsh is already the default shell"
+    exit 0
+else
+    echo "📟 Changing default shell to zsh"
+    chsh -s "/bin/zsh"
+fi
 
 # Install zsh dotfiles
+git clean -dfX zsh/
 stow --no-folding --target "${HOME}" zsh
