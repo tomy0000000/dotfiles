d48f33cc0f9279a2d075dba32f7cefb49a12e151
Tomy Hsieh <git@tomy.me>
2024-07-30 00:20:28 -0700

    🛠 fix: zshrc for apple silicon

--- zsh/.zenv/brew.zsh
+++ zsh/.zenv/brew.zsh
@@ -1,8 +1,18 @@
 #!/bin/zsh
 
-if command -v brew &>/dev/null; then
+UNAME_MACHINE="$(/usr/bin/uname -m)"
+if [[ "${UNAME_MACHINE}" == "arm64" ]]; then
+    # On ARM macOS, this script installs to /opt/homebrew only
+    PRE_BREW_PREFIX="/opt/homebrew"
+else
+    # On Intel macOS, this script installs to /usr/local only
+    PRE_BREW_PREFIX="/usr/local"
+fi
+
+# Check if Homebrew is installed
+if [ -f "${PRE_BREW_PREFIX}/bin/brew" ]; then
     # Core environment variables
-    eval "$(/usr/local/bin/brew shellenv)"
+    eval "$(${PRE_BREW_PREFIX}/bin/brew shellenv)"
 
     # Completions
     export FPATH="${HOMEBREW_PREFIX}/share/zsh/site-functions:${FPATH}" # Brew formulae
