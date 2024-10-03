8079bfc02e9a6c513f726bd21259429a3dc42366
Tomy Hsieh <git@tomy.me>
2024-10-03 13:27:04 -0400

    ✨ feat: Update zsh replacement

--- zsh/.zalias/_replacement.zsh
+++ zsh/.zalias/_replacement.zsh
@@ -2,18 +2,24 @@
 
 # Use trash instead of rm on local
 if command -v "trash" &>/dev/null; then
-    alias grm="$(which rm)" # original rm
+    alias orm="$(which rm)" # original rm
     alias rm="trash"
 fi
 
 # Use colorized cat
 if command -v "ccat" &>/dev/null; then
-    alias gcat="$(which cat)" # original cat
+    alias ocat="$(which cat)" # original cat
     alias cat="ccat"
 fi
 
 # Use colorized less
 if command -v "cless" &>/dev/null; then
-    alias gless="$(which less)" # original less
+    alias oless="$(which less)" # original less
     alias less="cless"
+fi
+
+# Use GNU sed
+if command -v "gsed" &>/dev/null; then
+    alias msed="$(which sed)" # original macOS sed
+    alias sed="gsed"
 fi
