278c0c0258ab20f326f044df9494768c16a07af8
Tomy Hsieh <git@tomy.me>
2024-11-25 21:55:29 -0500

    ✨ feat: Update ipython startup script

--- python/.ipython/profile_default/startup/00-auto-import.py
+++ python/.ipython/profile_default/startup/00-auto-import.py
@@ -1,3 +1,16 @@
 from collections import Counter, defaultdict, deque
 from datetime import datetime
 from functools import reduce
+from pprint import pprint
+
+# Init some test data structures
+l = []
+d = {}
+c = Counter()
+dd = defaultdict(int)
+dq = deque()
+dt = datetime.now()
+s = ""
+r = range(10)
+i = int()
+t = tuple()
