185313b68be02466f58af352bc18b54764813b08
Tomy Hsieh <git@tomy.me>
2025-02-18 21:30:28 -0800

    ✨ feat: Enable IME switcher for Tower

--- hammerspoon/.hammerspoon/auto_ime.lua
+++ hammerspoon/.hammerspoon/auto_ime.lua
@@ -1,6 +1,7 @@
 hs.application.watcher.new(function(appName, eventType, appObject)
     if (eventType == hs.application.watcher.activated) then
-        ABC_APPS = { "Code", "Finder", "GitHub", "Grammarly", "iTerm2", "LeetCode", "Sublime Text", "Termius", "Xcode" }
+        ABC_APPS = { "Code", "Finder", "GitHub", "Grammarly", "iTerm2", "LeetCode", "Sublime Text", "Termius", "Tower",
+            "Xcode" }
         SHUANGPIN_APPS = {}
 
         if (hs.fnutils.contains(ABC_APPS, appName)) then
