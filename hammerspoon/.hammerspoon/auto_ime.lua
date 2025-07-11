a9ac107fc42ac673fb4906f1cc0464e41e70d0d3
Tomy Hsieh <git@tomy.me>
2025-07-11 02:40:46 -0700

    🗄 chore: Add hammerspoon debug log

--- hammerspoon/.hammerspoon/auto_ime.lua
+++ hammerspoon/.hammerspoon/auto_ime.lua
@@ -5,15 +5,19 @@ hs.application.watcher.new(function(appName, eventType, appObject)
         SHUANGPIN_APPS = {}
 
         if (hs.fnutils.contains(ABC_APPS, appName)) then
+            hs.console.printStyledtext("Switching to ABC for ", appName)
             hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
         elseif (hs.fnutils.contains(SHUANGPIN_APPS, appName)) then
+            hs.console.printStyledtext("Switching to Shuangpin for ", appName)
             hs.keycodes.currentSourceID("com.apple.inputmethod.TCIM.Shuangpin")
         elseif (appName == "LINE") then
             if (hs.window.focusedWindow():title() == "LINE") then
                 -- Change input method to ShuangPin
+                hs.console.printStyledtext("Switching to Shuangpin for ", appName)
                 hs.keycodes.currentSourceID("com.apple.inputmethod.TCIM.Shuangpin")
             else
                 -- Change input method to English
+                hs.console.printStyledtext("Switching to ABC for ", appName)
                 hs.keycodes.currentSourceID("com.apple.keylayout.ABC")
             end
         end
