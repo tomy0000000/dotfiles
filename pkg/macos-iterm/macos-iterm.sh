d302df46456ae1231b1a9e5061109e3d3f7182e9
Tomy Hsieh <git@tomy.me>
2026-01-04 17:15:09 -0800

    🗄 chore: Move `macos-iterm` package

--- pkg/macos-iterm/macos-iterm.sh
+++ pkg/macos-iterm/macos-iterm.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 # Import preferences from dotfiles
 # shellcheck disable=SC2088
-defaults write com.googlecode.iterm2 PrefsCustomFolder -string '~/.dotfiles/iterm'
+defaults write com.googlecode.iterm2 PrefsCustomFolder -string '~/.dotfiles/pkg/macos-iterm'
 defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true
