2f168f278fcdf647f1a891a539b95cd36c73f7f1
Tomy Hsieh <git@tomy.me>
2026-04-27 02:59:49 -0700

    🛠 [fix][starship] Fix ubuntu install

--- pkg/starship/debian-starship.sh
+++ pkg/starship/debian-starship.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+# This is only available in Ubuntu 25.10+
 # Install starship CLI via apt
-sudo apt-get update
-sudo apt-get install -y starship
+# sudo apt-get update
+# sudo apt-get install -y starship
+
+# Install using official script
+# https://starship.rs/guide/#%F0%9F%9A%80-installation
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
