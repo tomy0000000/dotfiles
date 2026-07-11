948d83f185911a0af6d7a02a8a58a2c2cd1aaf36
Tomy Hsieh <git@tomy.me>
2026-07-11 12:38:00 -0700

    🛠 [fix][macos-file-handler]

--- pkg/macos-file-handler/macos-file-handler.sh
+++ pkg/macos-file-handler/macos-file-handler.sh
@@ -8,11 +8,29 @@ set -euo pipefail
 # defaults export com.apple.LaunchServices/com.apple.launchservices.secure - | plutil -convert json -o - - | jq
 
 # URL Schemes
+duti -s com.googlecode.iterm2 ssh
+duti -s com.panic.Transmit ftp
 
-duti -s com.googlecode.iterm2 ssh shell
-duti -s com.panic.Transmit ftp all
-duti -s com.brave.Browser http all
-duti -s com.brave.Browser https all
+duti -s com.readdle.SparkDesktop mailto
+duti -s com.googlecode.iterm2 x-man-page
+duti -s com.googlecode.iterm2 telnet
+duti -s com.apple.iCal webcal
+# Note: Transmit doesn't mount smb/nfs/afp the way Finder does, set per request anyway
+duti -s com.panic.Transmit webdav
+duti -s com.panic.Transmit webdavs
+duti -s com.panic.Transmit sftp
+duti -s com.panic.Transmit smb
+duti -s com.panic.Transmit nfs
+duti -s com.panic.Transmit afp
+duti -s com.apple.ScreenSharing vnc
+duti -s com.apple.FaceTime tel
+
+# Remote Desktop (rdp): Microsoft Remote Desktop
+# duti -s com.microsoft.rdc.macos rdp
+
+# Default browser (http/https) can't be set via duti
+exist defaultbrowser || brew install defaultbrowser
+defaultbrowser browser
 
 # MIME Types
 
@@ -20,6 +38,7 @@ duti -s com.sublimetext.4 text/plain all
 # duti -s com.brave.Browser text/html viewer
 # duti -s com.microsoft.VSCode text/html editor
 duti -s com.microsoft.VSCode text/css all
+duti -s com.microsoft.VSCode application/toml all
 duti -s com.microsoft.VSCode application/json all
 duti -s com.microsoft.VSCode text/php all
 duti -s com.microsoft.VSCode application/x-yaml all
